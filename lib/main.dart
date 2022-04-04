c746417cd6c64f376f6f1806cc1b83bf365bc908
oscarbfs <oscar.borgesfs@gmail.com>
2022-04-04 15:47:36 -0300

    Componentes Iniciais

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
-import 'package:pomodoro/store/contador.store.dart';
+import 'package:pomodoro/pages/pomodoro.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,52 +14,46 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const HomePage(),
+      home: const Pomodoro(),
     );
   }
 }
 
-final store = ContadorStore();
+// import 'package:flutter_mobx/flutter_mobx.dart';
+// import 'package:pomodoro/store/contador.store.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+// final store = ContadorStore();
 
-  @override
-  Widget build(BuildContext context) {
-   return Scaffold(
-      appBar: AppBar(
-        title: Text('Contador'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Observer(builder: (_) => Text(
-              '${store.contador}',
-              style: Theme.of(context).textTheme.headline4,
-            ),)
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: store.incrementar,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), 
-    );
-  }
-}
+// class HomePage extends StatelessWidget {
+//   const HomePage({ Key? key }) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//    return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Contador'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             const Text(
+//               'You have pushed the button this many times:',
+//             ),
+//             Observer(builder: (_) => Text(
+//               '${store.contador}',
+//               style: Theme.of(context).textTheme.headline4,
+//             ),)
+//           ],
+//         ),
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: store.incrementar,
+//         tooltip: 'Increment',
+//         child: const Icon(Icons.add),
+//       ), 
+//     );
+//   }
+// }
---
+++ lib/pages/pomodoro.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:pomodoro/components/entrada_tempo.dart';
+
+class Pomodoro extends StatelessWidget {
+  const Pomodoro({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Text('Pomodoro'),
+        EntradaTempo(
+          titulo: 'Trabalho',
+          valor: 25,
+        ),
+        EntradaTempo(
+          titulo: 'Descanso',
+          valor: 5,
+        ),
+      ],
+    ));
+  }
+}
